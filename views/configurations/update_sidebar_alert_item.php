4ede273d261ba6ac2c8f5b54edf9dbb745764ef1
Oleg <109225168+frescoref@users.noreply.github.com>
2023-01-13 02:53:15 +0300

    Views: alert

---
+++ views/configurations/update_sidebar_alert_item.php
@@ -0,0 +1,14 @@
+<?php defined('ABSPATH') || exit; ?>
+
+<?php do_action('wc1c_admin_configurations_update_before_sidebar_alert_item_show'); ?>
+
+<div class="alert alert-<?php if(isset($args['type'])) printf('%s', wp_kses_post($args['type'])); ?>" style="max-width: 100%; <?php if(isset($args['css'])) printf('%s', wp_kses_post($args['css'])); ?>">
+    <?php if(isset($args['header'])): ?>
+        <?php printf('%s', wp_kses_post($args['header'])); ?>
+    <?php endif; ?>
+    <?php if(isset($args['body'])): ?>
+        <?php printf('%s', wp_kses_post($args['body'])); ?>
+    <?php endif; ?>
+</div>
+
+<?php do_action('wc1c_admin_configurations_update_after_sidebar_alert_item_show'); ?>
