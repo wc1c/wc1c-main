60a2eae49b15fd0d44c2676e08f4af762c78fd9f
Oleg <109225168+frescoref@users.noreply.github.com>
2023-01-18 21:01:53 +0300

    Fix: more

--- views/connection/init.php
+++ views/connection/init.php
@@ -2,16 +2,23 @@
 
 <?php
     $title = __('Connection', 'wc1c-main');
-    $title = apply_filters('wc1c_admin_settings_connect_title', $title);
+
+    if(has_filter('wc1c_admin_settings_connect_title'))
+    {
+        $title = apply_filters('wc1c_admin_settings_connect_title', $title);
+    }
 
     $text = sprintf
     (
-            '<p>%s</p> %s',
-            __('To receive support and official services, need to go through the authorization of external applications.', 'wc1c-main'),
-            __('Authorization of an external app occurs by going to the official WC1C and returning to the current site.', 'wc1c-main')
+        '<p>%s</p> %s',
+        __('To receive official services and improve of the plugin, you must go through the authorization of an external application.', 'wc1c-main'),
+        __('Authorization of an external app occurs by going to the official WC1C and returning to the current site.', 'wc1c-main')
     );
 
-    $text = apply_filters('wc1c_admin_settings_connect_text', $text);
+    if(has_filter('wc1c_admin_settings_connect_text'))
+    {
+        $text = apply_filters('wc1c_admin_settings_connect_text', $text);
+    }
 ?>
 
 <div class="wc1c-configurations-alert mb-2 mt-2">
