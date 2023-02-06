2bc5f6142e6a5b63567f78226bfe8cc6337f6b4d
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-07 01:26:24 +0300

    Init: CmlSchemaAbstract

---
+++ src/Schemas/Abstracts/Cml/CmlSchemaAbstract.php
@@ -0,0 +1,14 @@
+<?php namespace Wc1c\Main\Schemas\Abstracts\Cml;
+
+defined('ABSPATH') || exit;
+
+use Wc1c\Main\Schemas\Abstracts\SchemaAbstract;
+
+/**
+ * CmlSchemaAbstract
+ *
+ * @package Wc1c\Main\Abstracts\Cml
+ */
+abstract class CmlSchemaAbstract extends SchemaAbstract
+{
+}
