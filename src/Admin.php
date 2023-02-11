4f62f1da481f4c38b9acee404c860b468386bbfb
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-11 12:47:50 +0300

    Init: bootstrap js

--- src/Admin.php
+++ src/Admin.php
@@ -179,7 +179,8 @@ final class Admin
 	 */
 	public function initScripts()
 	{
-		wp_enqueue_script('wc1c_admin_tocbot', wc1c()->environment()->get('plugin_directory_url') . 'assets/js/tocbot/tocbot.min.js');
+        wp_enqueue_script('wc1c_admin_bootstrap', wc1c()->environment()->get('plugin_directory_url') . 'assets/js/bootstrap.bundle.min.js');
+        wp_enqueue_script('wc1c_admin_tocbot', wc1c()->environment()->get('plugin_directory_url') . 'assets/js/tocbot/tocbot.min.js');
 		wp_enqueue_script('wc1c_admin_main', wc1c()->environment()->get('plugin_directory_url') . 'assets/js/admin.js');
 	}
 
