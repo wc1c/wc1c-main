21effac31c3fceea9cd5229602cebec453ed3a00
Oleg <109225168+frescoref@users.noreply.github.com>
2023-02-22 22:03:20 +0300

    Fix: more

--- views/helps/main.php
+++ views/helps/main.php
@@ -1,12 +1,12 @@
 <?php defined('ABSPATH') || exit; ?>
 
 <?php
-    printf
-    (
-        '<p>%s %s</p>',
-        __('If no understand how Integration with 1C works, how to use and supplement it, can view the documentation.', 'wc1c-main'),
-        __('Documentation contains all kinds of resources such as code snippets, user guides and more.', 'wc1c-main')
-    );
+printf
+(
+    '<p>%s %s</p>',
+    __('If no understand how Integration with 1C works, how to use and supplement it, can view the documentation.', 'wc1c-main'),
+    __('Documentation contains all kinds of resources such as code snippets, user guides and more.', 'wc1c-main')
+);
 ?>
 
 <a href="https://wc1c.info/docs" target="_blank" class="button button-primary">
