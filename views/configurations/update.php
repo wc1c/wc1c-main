930f16e44b862705d64e84377d286749176ddb7c
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-04 01:00:11 +0300

    Remove backlink

--- views/configurations/update.php
+++ views/configurations/update.php
@@ -2,14 +2,9 @@
 
 <div class="row g-0">
     <div class="col-24 p-0">
-        <div class="px-2">
-            <?php
-                $label = __('Back to configurations list', 'wc1c-main');
-                wc1c()->views()->adminBackLink($label, $args['back_url']);
-            ?>
-        </div>
-        <div class="p-1 bg-white rounded-1 mb-3">
+        <div class="p-1 bg-white rounded-1 mb-3 mt-2">
             <?php do_action('wc1c_admin_configurations_update_header_show'); ?>
+            <?php //$args['back_url']; ?>
         </div>
     </div>
 </div>
