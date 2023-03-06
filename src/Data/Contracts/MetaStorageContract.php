a981d7be79ec865ce8d4f5fefede1885fcfb4656
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-07 02:41:04 +0300

    Use: Woplucore\Data

--- src/Data/Contracts/MetaStorageContract.php
+++ src/Data/Contracts/MetaStorageContract.php
@@ -2,49 +2,11 @@
 
 defined('ABSPATH') || exit;
 
-use Wc1c\Main\Data\Abstracts\DataAbstract;
-
 /**
  * MetaStorageContract
  *
  * @package Wc1c\Main\Data\Contracts
  */
-interface MetaStorageContract
+interface MetaStorageContract extends \Digiom\Woplucore\Data\Interfaces\MetaStorageInterface
 {
-	/**
-	 * Returns an array of meta for an object
-	 *
-	 * @param DataAbstract $data Data object
-	 *
-	 * @return array
-	 */
-	public function readMeta(&$data);
-
-	/**
-	 * Deletes meta based on meta ID
-	 *
-	 * @param DataAbstract $data Data object
-	 * @param object $meta Meta an object (containing at least ->id)
-	 *
-	 * @return array
-	 */
-	public function deleteMeta(&$data, $meta);
-
-	/**
-	 * Add new piece of meta.
-	 *
-	 * @param DataAbstract $data Data object
-	 * @param object $meta Meta object (containing ->key and ->value)
-	 *
-	 * @return int meta ID
-	 */
-	public function addMeta(&$data, $meta);
-
-	/**
-	 * Update meta
-	 *
-	 * @param DataAbstract $data Data object
-	 * @param object $meta Meta object (containing ->id, ->key and ->value)
-	 */
-	public function updateMeta(&$data, $meta);
 }
