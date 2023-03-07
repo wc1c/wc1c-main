a46d06bd7cb407117235aec7e11fb82ad682bcb5
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-07 04:24:13 +0300

    Use: Woplucore\Data

---
+++ src/Data/Interfaces/DataStorageInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Wc1c\Main\Data\Interfaces;
+
+defined('ABSPATH') || exit;
+
+/**
+ * DataStorageInterface
+ *
+ * @package Wc1c\Main\Data\Interfaces
+ */
+interface DataStorageInterface extends \Digiom\Woplucore\Data\Interfaces\DataStorageInterface
+{
+}
---
+++ src/Data/Interfaces/MetaStorageInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Wc1c\Main\Data\Interfaces;
+
+defined('ABSPATH') || exit;
+
+/**
+ * MetaStorageInterface
+ *
+ * @package Wc1c\Main\Data\Interfaces
+ */
+interface MetaStorageInterface extends \Digiom\Woplucore\Data\Interfaces\MetaStorageInterface
+{
+}
