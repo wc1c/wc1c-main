70225f477e32c9b6630894c0c13fba51b5ef9305
Oleg <109225168+frescoref@users.noreply.github.com>
2023-03-14 19:25:38 +0300

    Fix: more

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', function()
 {
-    tocbot.init({
-        tocSelector: '.wc1c-toc',
-        contentSelector: '.wc1c-toc-container',
-        headingSelector: 'h1, h2, h3, h4, h5',
-        hasInnerContainers: true,
-        listClass: 'list-group m-0',
-        linkClass: 'stretched-link',
-        listItemClass: 'list-group-item',
-        activeListItemClass: 'active',
-        headingsOffset: 55,
-        scrollSmoothOffset: -55,
-        positionFixedSelector: '.wc1c-sidebar-toc',
-        positionFixedClass: 'is-position-fixed position-sticky',
-    });
+    if(document.querySelector(".wc1c-toc"))
+    {
+        tocbot.init({
+            tocSelector: '.wc1c-toc',
+            contentSelector: '.wc1c-toc-container',
+            headingSelector: 'h1, h2, h3, h4, h5',
+            hasInnerContainers: true,
+            listClass: 'list-group m-0',
+            linkClass: 'stretched-link',
+            listItemClass: 'list-group-item',
+            activeListItemClass: 'active',
+            headingsOffset: 55,
+            scrollSmoothOffset: -55,
+            positionFixedSelector: '.wc1c-sidebar-toc',
+            positionFixedClass: 'is-position-fixed position-sticky',
+        });
+    }
 });
