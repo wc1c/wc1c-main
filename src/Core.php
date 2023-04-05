65ec7508cf5375bf9949212e80320ad3650a3ae0
Oleg <109225168+frescoref@users.noreply.github.com>
2023-04-05 11:53:59 +0300

    Fix: more

--- src/Core.php
+++ src/Core.php
@@ -475,25 +475,11 @@ final class Core extends CoreAbstract
 			$use_class_name = $default_class_name;
 		}
 
-		try
-		{
-			$receiver = new $use_class_name();
-		}
-		catch(Exception $e)
-		{
-			throw $e;
-		}
+		$receiver = new $use_class_name();
 
 		$receiver->register();
 
-		try
-		{
-			$this->setReceiver($receiver);
-		}
-		catch(Exception $e)
-		{
-			throw $e;
-		}
+		$this->setReceiver($receiver);
 	}
 
 	/**
