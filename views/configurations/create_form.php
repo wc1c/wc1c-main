9d96a21a0e7bf4d94d73f5213fb21eb6f441f21e
Frescoref <frescoref@gmail.com>
2023-06-13 00:17:35 +0300

    Fix: more

--- views/configurations/create_form.php
+++ views/configurations/create_form.php
@@ -20,24 +20,24 @@
         <div class="col-24 col-lg-7 p-0">
             <?php do_action('wc1c_admin_configurations_create_sidebar_before_show'); ?>
 
-            <div class="alert alert-primary border-0" style="max-width: 100%;">
+            <div class="card border-0 mt-0 p-0 mb-2 w-100" style="max-width: 100%;">
+                <div class="card-body p-3">
+                    <?php _e('Enter a name for the new configuration, select a scheme, and click the create configuration button.', 'wc1c-main'); ?>
+                </div>
+                <div class="card-footer p-3">
+                    <p class="submit p-0 m-0">
+                        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Create configuration', 'wc1c-main'); ?>">
+                    </p>
+                </div>
+            </div>
+
+            <div class="alert alert-primary border-0">
                 <h4 class="alert-heading mt-0 mb-1"><?php _e('More schemas for configurations', 'wc1c-main'); ?></h4>
 		        <?php _e('Additional schemes are available in extensions on the official website.', 'wc1c-main'); ?>
                 <br/>
                 <?php _e('Each exchange scheme has a unique algorithm of operation and purpose. You need to choose only the scheme you need.', 'wc1c-main'); ?>
                 <hr>
                 <?php _e('Site:', 'wc1c-main'); ?> <a target="_blank" href="//wc1c.info/extensions">wc1c.info/extensions</a>
-            </div>
-
-            <div class="card border-0 mt-0 p-0" style="max-width: 100%;">
-                <div class="card-body p-3">
-                   <?php _e('Enter a name for the new configuration, select a scheme, and click the create configuration button.', 'wc1c-main'); ?>
-	            </div>
-                <div class="card-footer p-3">
-                    <p class="submit p-0 m-0">
-                        <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Create configuration', 'wc1c-main'); ?>">
-                    </p>
-                </div>
             </div>
 
             <?php do_action('wc1c_admin_configurations_create_sidebar_after_show'); ?>
