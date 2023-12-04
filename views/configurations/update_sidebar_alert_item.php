7271f0e1b0f32c638c8b4b9cf82c31a2e9f859b1
Frescoref <frescoref@gmail.com>
2023-12-04 16:50:14 +0300

    Fix: more

--- views/configurations/update_sidebar_alert_item.php
+++ views/configurations/update_sidebar_alert_item.php
@@ -4,7 +4,7 @@
 
 <div class="border-0 p-2 alert alert-<?php if(isset($args['type'])) printf('%s', wp_kses_post($args['type'])); ?>" style="<?php if(isset($args['css'])) printf('%s', wp_kses_post($args['css'])); ?>">
     <?php if(isset($args['header'])): ?>
-        <?php printf('%s', wp_kses_post($args['header'])); ?>
+        <?php printf('<span class="fs-6">%s</span>', wp_kses_post($args['header'])); ?>
     <?php endif; ?>
     <?php if(isset($args['body'])): ?>
         <?php printf('%s', wp_kses_post($args['body'])); ?>
