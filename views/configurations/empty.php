8726fc07f45ad5f35c4b794a6e12791d7abbe0a1
Frescoref <frescoref@gmail.com>
2023-12-04 13:15:54 +0300

    Fix: language

--- views/configurations/empty.php
+++ views/configurations/empty.php
@@ -1,6 +1,12 @@
-<?php defined('ABSPATH') || exit;?>
+<?php defined('ABSPATH') || exit; ?>
 
-<h2><?php
+<div class="alert alert-primary fs-6">
+    <?php esc_html_e('To exchange data, at least one configuration must be created and configured.', 'wc1c-main'); ?>
+    <?php esc_html_e('The number of settings and method of data exchange in the configuration depends on the scheme selected for exchange during creation.', 'wc1c-main'); ?>
+</div>
+
+<h2 class="mt-0">
+<?php
 	if(!empty($_REQUEST['s']))
 	{
 		$search_text = wc_clean(wp_unslash($_REQUEST['s']));
@@ -8,10 +14,15 @@
 	}
     else
     {
-	    esc_html_e( 'Configurations not found.', 'wc1c-main' );
+	    esc_html_e('Configurations not found', 'wc1c-main');
     }
-?></h2>
+?>
+</h2>
 
-<a href="<?php echo esc_url_raw($args['url_create']); ?>" class="mt-2 btn-lg d-inline-block page-title-action">
+<p class="fs-6">
+    <?php esc_html_e('For flexible exchange distribution, an unlimited number of configurations can be created.', 'wc1c-main'); ?>
+    <?php esc_html_e('It is recommended to create at least two configurations: 1. To exchange nomenclature data, 2. To exchange orders data.', 'wc1c-main'); ?>
+</p>
+<a href="<?php echo esc_url_raw($args['url_create']); ?>" class="mt-2 mx-0 fs-6 btn-lg d-inline-block page-title-action">
     <?php _e('New configuration', 'wc1c-main'); ?>
 </a>
